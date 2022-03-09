1f7110ce62248ba70f205d97ef2b31b08559435c
pavan <pavanguntupally12@gmail.com>
2022-03-09 12:14:13 +0530

    changed the server listining port to default

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ dotenv.config()
 
 const app = express()
 
-app.listen(9000,()=>console.log(`Server listening on port ${process.env.PORT}`))
+app.listen(process.env.PORT,()=>console.log(`Server listening on port ${process.env.PORT}`))
 
 // async function createConnection(){
 //     const client = new MongoClient(process.env.MONGO_DB)
